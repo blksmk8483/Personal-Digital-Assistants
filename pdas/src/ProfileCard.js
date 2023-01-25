27692e532c6e9fc8b9883e454ff762c852ed3f45
Brandon Knight <knightbrandon1984@gmail.com>
2023-01-25 11:52:55 -0600

    added styling to the cards/images

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,19 +1,28 @@
-function ProfileCard({title, handle, image }) {
+function ProfileCard({ title, handle, image }) {
     // ------ I could write this either way, this just get the code lines down by a few more bits... ------
     // OPTION 1 - how I could write this
     // const title = props.title;
     // const handle = props.handle;
 
-     // OPTION 2 - how I could write this
+    // OPTION 2 - how I could write this
     // const { title, handle } = props;
 
     // OPTION 3 - I could list the variables I want inside the argument list on line 1
 
     return (
-        <div>
-            <img src={image} alt="profile card"/>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="profile card" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
         </div>
     );
 }
